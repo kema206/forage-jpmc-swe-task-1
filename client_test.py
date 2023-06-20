f0c87156f4f62972e99cc18eb56b742e8b9e4ff0
Kevin Mario <kevinmario2006@gmail.com>
2023-06-20 12:17:07 -0700

    made changes to client3.py and client_test.py

--- client_test.py
+++ client_test.py
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for i in quotes:
+      self.assertEqual(getDataPoint(i),(i['stock'],i['bid_price'],i['ask_price'],(i['bid_price']+i['ask_price'])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +17,18 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for i in quotes:
+      self.assertEqual(getDataPoint(i),(i['stock'],i['bid_price'],i['ask_price'],(i['bid_price']+i['ask_price'])/2))
 
 
   """ ------------ Add more unit tests ------------ """
-
-
+  def test_getDataPoint_calculatePriceEqualToAsk(self):
+    quotes = [
+      {'top_ask': {'price': 105.2, 'size': 45}, 'timestamp': '2019-02-11 19:06:30.572453', 'top_bid': {'price': 105.2, 'size': 219}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 118.68, 'size': 9}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 118.68, 'size': 73}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    for i in quotes:
+      self.assertEqual(getDataPoint(i),(i['stock'],i['bid_price'],i['ask_price'],(i['bid_price']+i['ask_price'])/2))
 
 if __name__ == '__main__':
     unittest.main()
